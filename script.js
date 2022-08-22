7c0e749025a83be055df963289e93c10116249e6
Vivek Lohar <99829065+LoharV@users.noreply.github.com>
2022-08-22 12:26:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -90,7 +90,7 @@ const displayResult = () => {
     timeTaken = (120 - time) / 100;
   }
   document.getElementById("wpm").innerText =
-    (userInput.value.length / 5 / timeTaken).toFixed(2) + " wpm";
+    (userInput.value.length / 10 / timeTaken).toFixed(4) + " wpm";
   document.getElementById("accuracy").innerText =
     Math.round(
       ((userInput.value.length - mistakes) / userInput.value.length) * 100
